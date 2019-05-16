1298e10241956c56da08440f3f03ea507e1e9838
karlxavier <kxdc1025@yahoo.com>
2019-05-16 11:21:10 +0400

    added authy.rb in initializers

--- app/controllers/patients/patients_controller.rb
+++ app/controllers/patients/patients_controller.rb
@@ -39,8 +39,7 @@ class Patients::PatientsController < ApplicationController
                          else
                               puts '*********** authy errors'
                               puts authy.errors
-                              puts @patient.email
-                              puts country_code
+           
                               flash.now[:mobile_errors] = "Authy API error. Please contact Administrator."
                               format.js { render action: "invalid_mobile" }
                          end
